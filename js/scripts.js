17ab185d9c44e3290d4c70e2888a2b3cc3fcdd0d
Student <example@example.com>
2019-01-18 12:22:58 -0800

    Add id's to each result. Add click function. Assign variables in js.

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(function()) {
+  $(".btn").click(function(event){
+    var pathOption = $("#path").val();
+    var placeOption = $("#place").val();
+    var companyOption = $("#company").val();
+    var levelOption = $("#level").val();
+    var activityOption = $("#activity").val();
+
+
+    
+
+
+
+    event.preventDefault();
+  });
+});
