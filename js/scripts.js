176c9b112bd54677df7a6e0dfb5d98996ebaa300
Student <example@example.com>
2019-01-18 15:53:14 -0800

    Add jQuery functions and fix option values to evaluate correctly and show the correct results.

--- js/scripts.js
+++ js/scripts.js
@@ -5,49 +5,72 @@ $(document).ready(function() {
     var companyOption = $("#company").val();
     var levelOption = $("#level").val();
     var activityOption = $("#activity").val();
+    var language = 0;
 
+    //
+    // if (pathOption === "1" && placeOption === "1" && companyOption === "1" && levelOption === "1" && activityOption === "1") {
+    //   language = 1;
+    // }
+    // if (pathOption === "2" && placeOption === "2" && companyOption === "2" && levelOption === "2" && activityOption === "2"){
+    //   language = 2;
+    // }
+    // if (pathOption === "3" && placeOption === "3" && companyOption === "3" && levelOption === "3" && activityOption === "3"){
+    //   language = 3;
+    // }
+    // if (pathOption === "4" && placeOption === "4" && companyOption === "4" && levelOption === "4" && activityOption === "4"){
+    //   language = 4;
+    // }
 
-
-    if (pathOption === "1" && placeOption === "1" && companyOption === "1" && levelOption === "1" && activityOption === "1") {
-      language = 1;
+    if (pathOption === "1"){
+      language += 1;
+    } else {
+      language += 10;
     }
-    if (pathOption === "2" && placeOption === "2" && companyOption === "2" && levelOption === "2" && activityOption === "2"){
-      language = 2;
+
+    if (placeOption === "1"){
+      language += 1;
+    } else {
+      language += 10;
     }
-    if (pathOption === "3" && placeOption === "3" && companyOption === "3" && levelOption === "3" && activityOption === "3"){
-      language = 3;
+
+    if (companyOption === "1"){
+      language += 1;
+    } else {
+      language += 10;
     }
-    if (pathOption === "4" && placeOption === "4" && companyOption === "4" && levelOption === "4" && activityOption === "4"){
-      language = 4;
+
+    if (levelOption === "1"){
+      language += 1;
+    } else {
+      language += 10;
+    }
+
+    if (activityOption === "1"){
+      language += 1;
+    } else {
+      language += 10;
     }
 
 
+    if (language >= 30){
+      $("#answer").fadeToggle();
+      $("#javaResult").fadeToggle();
+      $("#csharpResult").fadeToggle();
+    } else {
+      $("#answer").fadeToggle();
+      $("#rubyResult").fadeToggle();
+      $("#phpResult").fadeToggle();
+      $("#cssResult").fadeToggle();
+    }
 
-    
 
     $("#questionnaire").toggle();
-    $("#cssResult").fadeToggle();
-    $("#rubyResult").fadeToggle();
-    $("#phpResult").fadeToggle();
-    $("#javaResult").fadeToggle();
-    $("#csharpResult").fadeToggle();
+    // $("#cssResult").fadeToggle();
+    // $("#rubyResult").fadeToggle();
+    // $("#phpResult").fadeToggle();
+    // $("#javaResult").fadeToggle();
+    // $("#csharpResult").fadeToggle();
 
-
-
-
-
-
-    // var language = $("#path").val();
-    //
-    // switch (language) {
-    //   default:
-    //     text = "The learning track you should pursue is:"
-    //     $("#rubyResult").show();
-    //     break;
-    //   case 1:
-    //     text = "The learning track you should pursue is:"
-    //     $("#phpResult").show();
-    // }
 
 
     event.preventDefault();
